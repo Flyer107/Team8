465dd491b1bf9c9c390ee587bfe43b6b295e0d85
Miguel <musngimiguel@gmail.com>
2021-10-03 09:51:34 -0400

    Revert back to initial state

--- Projects/P2/src/Ghost.java
+++ Projects/P2/src/Ghost.java
@@ -6,13 +6,11 @@ public class Ghost{
 	String myName;
 	Location myLoc;
 	Map myMap;
-        Random rand;
 
 	public Ghost(String name, Location loc, Map map) {
 		this.myLoc = loc;
 		this.myName = name;
 		this.myMap = map;
-		this.rand = new Random();
 	}
 
 	public ArrayList<Location> get_valid_moves() {
@@ -20,14 +18,7 @@ public class Ghost{
 	}
 
 	public boolean move() {
-	    ArrayList<Location> moves = get_valid_moves();
-	    if(moves.size() > 0) {
-		int randIndex = rand.nextInt(moves.size());
-		this.myLoc = moves.get(randIndex);
-		return true;
-	    } else {
-		return false;
-	    }
+	        return false;
 	}
 
 	public boolean is_pacman_in_range() { 
