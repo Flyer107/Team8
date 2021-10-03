93f63dd8ca9557d03e45649aed40027f47b24333
justinrobinson1020 <justin.w.robinson1020@gmail.com>
2021-10-03 18:08:41 -0400

    add pacman-get_valid_moves

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -2,31 +2,42 @@ import java.util.HashSet;
 import java.util.ArrayList;
 import javax.swing.JComponent;
 
-public class PacMan{
-	String myName;
-	Location myLoc;
-	Map myMap;
-	Location shift; 
+public class PacMan {
+  String myName;
+  Location myLoc;
+  Map myMap;
+  Location shift;
 
-	public PacMan(String name, Location loc, Map map) {
-		this.myLoc = loc;
-		this.myName = name;
-		this.myMap = map;
-	}
+  public PacMan(String name, Location loc, Map map) {
+    this.myLoc = loc;
+    this.myName = name;
+    this.myMap = map;
+  }
 
-	public ArrayList<Location> get_valid_moves() {
-		return null;	
-	}
+  public ArrayList<Location> get_valid_moves() {
+    ArrayList<Location> validMoves = new ArrayList<Location>();
 
-	public boolean move() {
-		return false;
-	}
+    for (int x = -1; x < 2; x++) {
+      for (int y = -1; y < 2; y++) {
+        Location loc = this.myLoc.shift(x, y);
+        if (!this.myMap.getLoc(loc).contains(Map.Type.WALL) && !this.myMap.getLoc(loc).contains(Map.Type.GHOST)) {
+          validMoves.add(loc);
+        }
+      }
+    }
 
-	public boolean is_ghost_in_range() { 
-		return false;
-	}
+    return validMoves;
+  }
 
-	public JComponent consume() { 
- 		return null;
-	}
+  public boolean move() {
+    return false;
+  }
+
+  public boolean is_ghost_in_range() {
+    return false;
+  }
+
+  public JComponent consume() {
+    return null;
+  }
 }
