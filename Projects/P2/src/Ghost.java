1b3cb084980d1a316106a99b0943e9432c54ce73
justinrobinson1020 <justin.w.robinson1020@gmail.com>
2021-10-03 18:04:17 -0400

    add ghost-get_valid_moves

--- Projects/P2/src/Ghost.java
+++ Projects/P2/src/Ghost.java
@@ -13,7 +13,18 @@ public class Ghost{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;
+    ArrayList<Location> validMoves = new ArrayList<Location>();
+
+    for (int x = -1; x < 2; x++) {
+      for (int y = 1; y < 2; y++) {
+        Location loc = this.myLoc.shift(x, y);
+        if (!this.myMap.getLoc(loc).contains(Map.Type.WALL)) {
+          validMoves.add(loc);
+        }
+      }
+    }
+
+    return validMoves;
 	}
 
 	public boolean move() {
