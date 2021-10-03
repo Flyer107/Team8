d7c565d5e7e1cb6ce489cc00ab17db942a6b5ea3
yuexia <yuexia718@gmail.com>
2021-10-02 20:02:54 -0400

    Completed is_pacman_in_range function

--- Projects/P2/src/Ghost.java
+++ Projects/P2/src/Ghost.java
@@ -21,6 +21,35 @@ public class Ghost{
 	}
 
 	public boolean is_pacman_in_range() { 
+		Location pacman_top_left = myLoc.shift(-1, 1);
+	    Location pacman_top = myLoc.shift(0, 1);
+		Location pacman_top_right = myLoc.shift(1, 1);
+		Location pacman_left = myLoc.shift(-1, 0);
+		Location pacman_right = myLoc.shift(1, 0);
+		Location pacman_down_left = myLoc.shift(-1, -1);
+		Location pacman_down = myLoc.shift(0, -1);
+		Location pacman_down_right = myLoc.shift(1, -1);
+
+		ArrayList<Location> possible_locations = new ArrayList<Location>(8);
+		possible_locations.add(pacman_top_left);
+		possible_locations.add(pacman_top);
+		possible_locations.add(pacman_top_right);
+		possible_locations.add(pacman_left);
+		possible_locations.add(pacman_right);
+		possible_locations.add(pacman_down_left);
+		possible_locations.add(pacman_down);
+		possible_locations.add(pacman_down_right);
+		
+		
+		HashSet<Map.Type> hash = new HashSet<Map.Type>();
+		hash.add(Map.Type.PACMAN);
+
+		for (int i = 0; i < possible_locations.size(); i++) {
+			if (this.myMap.getLoc(possible_locations.get(i)).equals(hash)) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
