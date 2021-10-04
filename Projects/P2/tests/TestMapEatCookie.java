3e19e937196c2700f68190b43ec88ba8578f3f93
justinrobinson1020 <justin.w.robinson1020@gmail.com>
2021-10-04 14:16:57 -0400

    add map-eat_cookie tests

--- Projects/P2/tests/TestMapEatCookie.java
+++ Projects/P2/tests/TestMapEatCookie.java
@@ -3,8 +3,25 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestMapEatCookie {
-	
-	public void testMapEatCookie() {
-		return null;
-	}
+
+  public void testMapEatCookie() {
+    Map testMap = new Map();
+    Location testLoc = new Location(1, 1);
+    CookieComponent testCoookie = new CookieComponent(1, 1, 20);
+    testMap.add("tok_x1_y1", testLoc, testCoookie, Map.Type.COOKIE);
+
+    //Eating a cookie returns the correct component
+    CookieComponent returnedCookie = testMap.eatCookie("tok_x1_y1");
+    assertEqual(testCoookie, returnedCookie);
+
+    //Cookie is no longer at its location
+    assertTrue(!testMap.getLoc(testLoc).contains(Map.Type.COOKIE));
+
+    //Player is awarded a point
+    assertEqual(testMap.getCookies(), 1);
+
+    //Attempting to eat a nonexistent cookie returns null
+    returnedCookie = testMap.eatCookie("tok_x2_y2");
+    assertEqual(returnedCookie, null);
+  }
 }
