ea22c8f1f435a1ac52f945dc46ffbcab18fb81d2
Miguel <musngimiguel@gmail.com>
2021-10-04 18:03:21 -0400

    Did test and updated this.myLoc when PacMan.move()is called

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -26,6 +26,7 @@ public class PacMan{
 		ArrayList<Location> moves = this.get_valid_moves();
 		if(moves.size() > 0) {
 			int randIndex = rand.nextInt(moves.size());
+			this.myLoc = moves.get(randIndex);
 			return this.myMap.move(this.myName, moves.get(randIndex), Map.Type.PACMAN);
 		} else {
 			return false;
