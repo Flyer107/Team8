10cc326ac07579fc47b42e19298c9191db48bb19
Miguel <musngimiguel@gmail.com>
2021-10-04 17:37:25 -0400

    Did the test for TestMapMove

--- Projects/P2/tests/TestMapMove.java
+++ Projects/P2/tests/TestMapMove.java
@@ -5,6 +5,19 @@ import java.io.*;
 public class TestMapMove {
 
 	public void testMapMove() {
-		return null;
+	    // Initializations
+	    Map m = new Map(2);
+	    Location initLoc = new Location(0, 0);
+	    Location destLoc = new Location(0, 1);
+	    String name = "P1";
+	    PacManComponent pakupaku = new PacManComponent(0, 0, 1);
+	    m.add(name, initLoc, pakupaku, Map.Type.PACMAN);
+
+	    assertTrue(m.move(name, destLoc, Map.Type.PACMAN));
+	    assertTrue(m.getLoc(initLoc).contains(Map.Type.EMPTY));
+	    assertTrue(!m.getLoc(destLoc).contains(Map.Type.EMPTY)); 
+	    assertTrue(m.getLoc().contains(Map.Type.PACMAN));
+
+	    assertFalse(m.move("ioehoihs", destLoc, Map.Type.GHOST));
 	}
 }
