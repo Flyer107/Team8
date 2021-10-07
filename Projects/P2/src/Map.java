dc14c26b747cdd6695508bf34c2438f1894fb7f3
Miguel <musngimiguel@gmail.com>
2021-10-07 16:34:37 -0400

    Fixed EMPTY to Type.EMPTY

--- Projects/P2/src/Map.java
+++ Projects/P2/src/Map.java
@@ -57,11 +57,11 @@ public class Map{
 		//use the setLocation method for the component to move it to the new location
 	        if(locations.containsKey(name) && components.containsKey(name)) {
 		        field.get(locations.get(name)).remove(type); // Remove the type of old location
-			if (field.get(locations.get(name)).isEmpty()) field.get(locations.get(name)).put(EMPTY); // If old location is empty, add EMPTY to hashset
+			if (field.get(locations.get(name)).isEmpty()) field.get(locations.get(name)).put(Type.EMPTY); // If old location is empty, add EMPTY to hashset
 			locations.replace(name, loc); // Update locations
 			components.get(name).setLocation(loc.x, loc.y); // update components
 			if (!field.containsKey(loc)) field.put(loc, new HashSet<Type>()); // copied from add function
-			if (field.get(loc).contains(EMPTY)) field.get(loc).remove(EMPTY); // If it was empty before, remove EMPTY in Hashset
+			if (field.get(loc).contains(Type.EMPTY)) field.get(loc).remove(Type.EMPTY); // If it was empty before, remove EMPTY in Hashset
 			field.get(loc).add(type); // copied from add function
 			return true;
 		} else {
