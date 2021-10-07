f562b2a1c58f5712e8e9df793301a6c361f82ffd
lucastucker-umd <ltuck99@umd.edu>
2021-10-07 17:04:06 -0400

    fixed syntax errors in pacman tests

--- Projects/P2/tests/TestPacManValidMoves.java
+++ Projects/P2/tests/TestPacManValidMoves.java
@@ -29,7 +29,7 @@ public class TestPacManValidMoves extends TestCase {
           testMap.add("ghost", loc, ghost, Map.Type.GHOST);
         } else {
           CookieComponent tok = new CookieComponent(loc.x, loc.y, 20);
-          myMap.add("tok_x" + row + "_y" + col, loc, tok, Map.Type.COOKIE);
+          testMap.add("tok_x" + row + "_y" + col, loc, tok, Map.Type.COOKIE);
         }
       }
     }
