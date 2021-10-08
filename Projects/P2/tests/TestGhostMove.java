2ed4cdc7844c4242b4a196380dd67b5918b2ed2f
Miguel <musngimiguel@gmail.com>
2021-10-08 12:34:29 -0400

    Added necessary components for the map

--- Projects/P2/tests/TestGhostMove.java
+++ Projects/P2/tests/TestGhostMove.java
@@ -6,15 +6,11 @@ import java.util.ArrayList;
 public class TestGhostMove extends TestCase {
 
 	public void testGhostMove() throws FileNotFoundException{
-	    Map noSpace = new Map(1);
-	    Location loc = new Location(0,0);
-	    Ghost fuyufuyu = new Ghost("fuyufuyu", loc, noSpace);
-	    assertFalse(fuyufuyu.move());
-
 	    // Test for moved location
 	    Map m = new Map(3);
 	    Location initLoc = new Location(1,1);
 	    Ghost fuyu = new Ghost("fuyu", initLoc, m);
+	    m.add("fuyu", initLoc, new GhostComponent(1, 1, 1), Map.Type.GHOST);
 	    ArrayList<Location> validMoves = fuyu.get_valid_moves();
 	    assertTrue(fuyu.move());
 	    assertTrue(validMoves.contains(fuyu.myLoc));
--- Projects/P2/tests/TestPacManMove.java
+++ Projects/P2/tests/TestPacManMove.java
@@ -6,16 +6,11 @@ import java.util.ArrayList;
 public class TestPacManMove extends TestCase {
 
 	public void testPacManMove() throws FileNotFoundException{
-	    // Test for no moves
-	    Map noSpace = new Map(1);
-	    Location loc = new Location(0,0);
-	    PacMan pakupaku = new PacMan("pakupaku", loc, noSpace);
-	    assertFalse(pakupaku.move());
-
 	    // Test for moved location
 	    Map m = new Map(3);
 	    Location initLoc = new Location(1,1);
 	    PacMan paku = new PacMan("paku", initLoc, m);
+	    m.add("paku", initLoc, new PacManComponent(1, 1, 1), Map.Type.PACMAN);
 	    ArrayList<Location> validMoves = paku.get_valid_moves();
 	    assertTrue(paku.move());
 	    assertTrue(validMoves.contains(paku.myLoc));
